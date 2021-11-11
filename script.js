9451a05c39a82263348a64de74ed75ed5da63392
PavloMartynyak <pavlomart2002@gmail.com>
2021-11-11 17:59:24 +0200

    1 lesson (homework)

---
+++ script.js
@@ -0,0 +1,159 @@
+{
+    let a = 'hello';
+    a = 'hi';
+    console.log(a);
+    alert(a);
+    document.write(a);
+}
+
+{
+    let b = 'owu';
+    b = 'Okten';
+    console.log(b);
+    alert(b);
+    document.write(b);
+}
+
+{
+    let c = 'com';
+    c = 'dot';
+    console.log(c);
+    alert(c);
+    document.write(c);
+}
+
+{
+    let d = 'ua';
+    d = 'pl';
+    console.log(d);
+    alert(d);
+    document.write(d);
+}
+
+{
+    let e = 1;
+    e = 3;
+    console.log(e);
+    alert(e);
+    document.write(e);
+}
+
+{
+    let f = 10;
+    f = 8;
+    console.log(f);
+    alert(f);
+    document.write(f);
+}
+
+{
+    let g = -999;
+    g = -666;
+    console.log(g);
+    alert(g);
+    document.write(g);
+}
+
+{
+    let h = 123;
+    h = 321;
+    console.log(h);
+    alert(h);
+    document.write(h);
+}
+
+{
+    let i = 3.14;
+    i = 14.3;
+    console.log(i);
+    alert(i);
+    document.write(i);
+}
+
+{
+    let j = 2.7;
+    j = 3.8;
+    console.log(j);
+    alert(j);
+    document.write(j);
+}
+
+{
+    let k = 16;
+    k = 14;
+    console.log(k);
+    alert(k);
+    document.write(k);
+}
+
+
+{
+    let l = true;
+    l = 5 > 6;
+    console.log(l);
+    alert(l);
+    document.write(l);
+}
+
+{
+    let m = 'false';
+    m = 4 < 7;
+    console.log(m);
+    alert(m);
+    document.write(m);
+}
+
+
+
+
+
+
+let firstName = 'Pavlo';
+let middleName = 'Andriyovych';
+let lastName = 'Martyniak';
+
+let me = [firstName, middleName, lastName];
+console.log(me);
+
+
+
+
+let name = prompt('Name');
+let midlleName = prompt('midl');
+let age = prompt('age');
+
+console.log(`Вітаємо ${name} ${midlleName} тобі ${age} років`);
+
+
+
+{
+    let a = 100;
+    console.log(typeof (a));
+}
+
+{
+    let b = '100';
+    console.log(typeof (b));
+}
+
+{
+    let c = true;
+    console.log(typeof(c))
+}
+
+
+
+
+console.log(5 !== 6);
+console.log(5 === 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+
+console.log(10 === 10);
+console.log(10 >= 10);
+console.log(10 > 10);
+console.log(10 !== 10);
+console.log(10 < 10);
+
+console.log(123 !== 123);
+console.log(123 === 123);
