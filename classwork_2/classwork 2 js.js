39552966fff7332eb0b8f869493e208cd7001b32
PavloMartynyak <pavlomart2002@gmail.com>
2021-11-15 17:40:23 +0200

    hw2

---
+++ classwork_2/classwork 2 js.js
@@ -0,0 +1,88 @@
+// let numeric1 = +prompt('Введіть число');
+// let numeric2 = +prompt('Введіть інше число');
+// let result = numeric1;
+// let result2 = numeric2;
+// let result3 = 'Числа рівні';
+// if (numeric1 > numeric2){
+//     console.log((result))
+// }else if (numeric1 < numeric2){
+//     console.log(result2)
+// }else {
+//     console.log(result3)
+// }
+
+
+
+// let flatsNum = prompt('Введіть номер квартири');
+// let porch1 = "перший під'їзд";
+// let porch2 = "другий під'їзд";
+// let porch3 = "третій під'їзд";
+// if (flatsNum <=20){
+//     console.log(porch1)
+// }else if (flatsNum > 20 && flatsNum <=48){
+//     console.log(porch2)
+// }else if (flatsNum > 48 && flatsNum <=90){
+//     console.log(porch3)
+// }else {
+//     console.log("Номер квартири невірний")
+// }
+
+
+
+
+
+// let number  = +prompt('Введіть ваше число');
+// if (number === 10){
+//     console.log('Вірно')
+// }else {
+//     console.log('Невірно')
+// }
+
+
+
+// let x;
+// if (typeof x === 'number'){
+//     console.log((1))
+// }else if (typeof x === 'string'){
+//     console.log(2)
+// }else if (typeof x === 'boolean'){
+//     console.log(3)
+// }else if (typeof x === 'object'){
+//     console.log(4)
+// }else {
+//     console.log('значення невірне')
+// }
+
+
+
+// let message = prompt('Яка температура на дворі?')
+// if (message >= '+10' && message <= '+21'){
+//     console.log('ми йдемо на навчання')
+// }else {
+//     console.log ('сидимо вдома і вчимося ОНЛАЙН')
+// }
+
+
+
+
+
+// let game = +prompt('Для того щоб забрати приз введіть число від 1 до 5');
+// switch (game){
+//     case(1):
+//         console.log('Ви виграли Авто')
+//         break
+//     case (2):
+//         console.log('Ви виграли Мото')
+//         break
+//     case (3):
+//         console.log('Ви виграли телефон')
+//         break
+//     case (4):
+//         console.log('Ви виграли самокат')
+//         break
+//     case (5):
+//         console.log('Ви виграли годинник')
+//         break
+//     default:
+//         console.log('Ви ввели невірне число, ви програли!')
+// }
---
+++ homework 2/homework2 js.js
@@ -0,0 +1,143 @@
+// let time = prompt('Введіть число від 0 до 59');
+// let parts = ['the first part', 'the second part', 'the third part', 'the fourth part'];
+//
+// if (time < 14) {
+//     console.log(parts[0])
+// }
+// else if (time >=14 && time <=29 ) {
+// console.log(parts[1])
+// }
+// else if (time >=29 && time <=44 ) {
+//     console.log(parts[2])
+// }
+// else if (time >=44 && time <=59 ) {
+//     console.log(parts[3])
+// }else {
+//     console.log('wrong time')
+// };
+
+
+
+
+
+
+// let day = prompt('Для того щоб взнати до якої декади відноситься день, просто впишіть його');
+// let decade = ['the first decade', 'the second decade', 'the third decade']
+//
+// if (day <=10){
+//     console.log(decade[0])
+// }
+// else if (day >10 && day <=20){
+//     console.log(decade[1])
+// }
+// else if (day > 20 && day <=31){
+//     console.log(decade[2])
+// }else {
+//     console.log('wrong day')
+// }
+
+
+
+
+
+// let test1 = 20;
+// let result;
+// if (test1 > 5){
+//     result = 'Вірно!'
+// }else {
+//     result = 'Невірно!'
+// }
+// console.log(result);
+//
+//
+// let test2 = 42;
+// let result2;
+// if (test2 < 14){
+//     result2 = 'Вірно!'
+// }else {
+//     result2 = 'Невірно!'
+// }
+// console.log(result2);
+
+
+
+
+
+// let a = 1;
+// let result3;
+// if ( a === 0 ){
+//     result3 = 'Невірно'
+// }else {
+//     result3 = 'Вірно'
+// }
+// console.log(result3);
+
+// let a = 0;
+// let result4;
+// if ( a === 0 ){
+//     result4 = 'Невірно'
+// }else {
+//     result4 = 'Вірно'
+// }
+// console.log(result4);
+
+
+// let a = -3;
+// let result5;
+// if ( a === 0 ){
+//     result5 = 'Невірно'
+// }else {
+//     result5 = 'Вірно'
+// }
+// console.log(result5);
+
+
+
+
+// let week = +prompt('Виберіть день тижня')
+//
+// switch (week){
+//     case (1):
+//         console.log('Прочитати 50 сторінок книги')
+//         break;
+//     case (2):
+//         console.log('Пробігти 5 км');
+//         break;
+//     case (3):
+//         console.log('Сходити в кіно');
+//         break;
+//     case (4):
+//         console.log('Практика JS');
+//         break;
+//     case (5):
+//         console.log('Провести час з рідними');
+//         break;
+//     case (6):
+//         console.log('Практика English');
+//         break;
+//     case (7):
+//         console.log('Відпочинок');
+//         break;
+// };
+
+
+
+
+// let year = +prompt('Введіть який сьогодні рік');
+// if(year % 4 === 0){
+//     console.log('Рік ВИСОКОСНИЙ');
+// }else {
+//     console.log('УРА! Наш рік нормальний');
+// };
+
+
+
+
+// let nameJS = prompt('Яка офіційна назва JavaScript?');
+// let result;
+//
+// if (nameJS === 'ECMAscript'){
+//     console.log('Вірно');
+// }else {
+//     console.log('Незнаєте? ECMAscript')
+// };
